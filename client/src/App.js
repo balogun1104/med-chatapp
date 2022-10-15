22ab8d22ff68aef9fdb06de67f746bf83e53315b
Balogun Razak <razak2free@gmail.com>
2022-10-16 00:17:58 +0100

    16/10/22

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const apiKey = 'emxdqfhehxh6';
+
+const cleint = StreamChat.getInstance(apiKey)
 
 const App = () => {
   return (
-    <div>
-      <h1>THis is s</h1>
+    <div className='app_wrapper'>
+        <Chat client={client} theme='team light'>
+            <ChannelListContainer />
+            <ChannelContainer />
+        </Chat>
     </div>
   )
 }
