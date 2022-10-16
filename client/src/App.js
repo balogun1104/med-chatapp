d5fe8b016b6f10100e24859986b99579b046222a
Balogun Razak <razak2free@gmail.com>
2022-10-16 01:15:04 +0100

    sideBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 import { StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
@@ -11,7 +12,7 @@ const client = StreamChat.getInstance(apiKey)
 
 const App = () => {
   return (
-    <div className='app_wrapper'>
+    <div className='app__wrapper'>
         <Chat client={client} theme='team light'>
             <ChannelListContainer />
             <ChannelContainer />
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react'
+import Cookies from 'universal-cookie';
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from 'stream-chat-react';
+import HospitalIcon from '../assets/hospital.png'
+import LogoutIcon from '../assets/logout.png'
+
+
+const SideBar = () => (
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={HospitalIcon} alt="Hospital" width='30' />
+      </div>
+    </div>
+
+    <div className="channel-list__sidebar__icon2">
+      <div className="icon1__inner">
+        <img src={LogoutIcon} alt="Logout" width='30' />
+      </div>
+    </div>
+
+  </div>
+)
 
 const ChannelListContainer = () => {
   return (
-    <div>
-      ChannelListContainer
-    </div>
+    <>
+     <SideBar />
+    </>
   )
 }
 
