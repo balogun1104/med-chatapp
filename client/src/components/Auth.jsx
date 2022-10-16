01376ca6c4be3f529cf6f2bd7961509df8796ef4
Balogun Razak <razak2free@gmail.com>
2022-10-16 18:34:27 +0100

    package update

---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import Cookies from 'universal-cookie';
+import axois from 'axios';
+
+const Auth = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Auth
