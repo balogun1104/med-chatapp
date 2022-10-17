acae9ea06175e33c27363fe2240db4be3ffb890a
Balogun Razak <razak2free@gmail.com>
2022-10-17 04:37:03 +0100

    FE(summary)

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelContainer, ChannelListContainer } from './components'
+import { ChannelContainer, ChannelListContainer, Auth } from './components'
 
 const apiKey = 'emxdqfhehxh6';
 
-const client = StreamChat.getInstance(apiKey)
+const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
 
 const App = () => {
+  if (!authToken) return <Auth />
   return (
     <div className='app__wrapper'>
         <Chat client={client} theme='team light'>
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -2,10 +2,134 @@ import React, {useState} from 'react';
 import Cookies from 'universal-cookie';
 import axois from 'axios';
 
+import signInImage from '../assets/signup.jpg'
+
+const initialState = {
+    fullName: '',
+    userName: '',
+    password: '',
+    confirmPassword: '',
+    phoneNumber: '',
+    avatarURL: '',
+}
 const Auth = () => {
+    const [isSignup, setIsSignup] = useState(true)
+    const [form, setForm] = useState(initialState)
+
+    const handleChange = (event) => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(form)
+    }
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup)
+    }
   return (
-    <div>
-      
+    <div className='auth__form-container'>
+        <div className="auth__form-container_fields">
+            <div className="auth__form-container_fields-content">
+                <p>{isSignup ? 'sign Up' : 'sign In'}</p>
+                <form onSubmit={handleSubmit}>
+                    {isSignup && (
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='fullName'>Full Name</label>
+                            <input
+                             type="text"
+                             name='fullName'
+                             placeholder='Full Name'
+                             onChange={handleChange}
+                             required
+                             />
+                        </div>
+                    )}
+                    
+                    <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='username'>Username</label>
+                            <input
+                             type="text"
+                             name='UserName'
+                             placeholder='Username'
+                             onChange={handleChange}
+                             required
+                             />
+                             {/* //username */}
+                        </div>
+                        {isSignup && (
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='phoneNumber'>Phone Number</label>
+                            <input
+                             type="tel"
+                             name='phoneNumber'
+                             placeholder='Phone Number'
+                             onChange={handleChange}
+                             required
+                             />
+                        </div>
+                    )}
+
+                        {isSignup && (
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='avatarURL'>Avatar URL</label>
+                            <input
+                             type="text"
+                             name='avatarURL'
+                             placeholder='Avatar URL'
+                             onChange={handleChange}
+                             required
+                             />
+                        </div>
+                    )}
+
+                     <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='password'>Password</label>
+                            <input
+                             type="password"
+                             name='password'
+                             placeholder='password'
+                             onChange={handleChange}
+                             required
+                             />
+                        </div>
+
+                        {isSignup && (
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor='password'>Confirm Password</label>
+                            <input
+                             type="password"
+                             name='comfirmPassword'
+                             placeholder='password'
+                             onChange={handleChange}
+                             required
+                             />
+                        </div>
+                        )}
+
+                        <div className="auth__form-container_fields-content_button">
+                            <button>
+                                {isSignup ? 'Sign Up' : 'Sign In'}
+                            </button>
+                        </div>
+                </form>
+                <div className="auth__form-container_fields-account">
+                    <p>
+                        {isSignup 
+                        ? 'Already Have An Account? ' 
+                        : "Don't Have An Account? "
+                        }
+                        <span onClick={switchMode}>
+                            {isSignup ? 'Sign In' : 'Sign Up'}
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div className="auth__form-container_image">
+            <img src={signInImage} alt="sign in" />
+        </div>
     </div>
   )
 }
