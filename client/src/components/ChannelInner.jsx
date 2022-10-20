29708792d5c9504cf8cd4bdc290656faf12fba7c
Balogun Razak <razak2free@gmail.com>
2022-10-20 19:31:00 +0100

    ChannelContainer

---
+++ client/src/components/ChannelInner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChannelInner = () => {
+  return (
+    <div>
+      Channel Inner
+    </div>
+  )
+}
+
+export default ChannelInner
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -5,8 +5,9 @@ import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import HospitalIcon from '../assets/hospital.png'
 import LogoutIcon from '../assets/logout.png'
 
+const cookies = new Cookies() ;  
 
-const SideBar = () => (
+const SideBar = ({logout}) => (
   <div className="channel-list__sidebar">
     <div className="channel-list__sidebar__icon1">
       <div className="icon1__inner">
@@ -15,7 +16,7 @@ const SideBar = () => (
     </div>
 
     <div className="channel-list__sidebar__icon2">
-      <div className="icon1__inner">
+      <div className="icon1__inner" onClick={logout}>
         <img src={LogoutIcon} alt="Logout" width='30' />
       </div>
     </div>
@@ -29,9 +30,20 @@ const CompanyHeader = () => (
 )
 
 const ChannelListContainer = () => {
+  const logOut = () => {
+    cookies.remove('token');
+    cookies.remove('username');
+    cookies.remove('fullName');
+    cookies.remove('userId');
+    cookies.remove('phoneNumber');
+    cookies.remove('avatarURL');
+    cookies.remove('hashedPassword');
+
+    window.location.reload(); 
+  }
   return (
     <>
-     <SideBar />
+     <SideBar logout={logOut}/>
      <div className="channel-list__list__wrapper">
       <CompanyHeader />
       <ChannelSearch />
