e83c0fb78e91983ecbf420efc8e50edb20974fa8
Balogun Razak <razak2free@gmail.com>
2022-10-21 20:14:51 +0100

    upgrade

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { Channel, useChatContext } from 'stream-chat-react';
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './'
 
-const ChannelContainer = () => {
+const ChannelContainer = ({ isCreacting, setIsCreating, isEditing, setIsEditing, createType }) => {
+  const { channel } = useChatContext();
+
+  if(isCreacting) {
+    return (
+      <div className="channel__container">
+        <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+      </div>
+    )
+  }
+
+  if(isEditing) {
+    return (
+      <div className="channel__container">
+      <EditChannel setIsEditing={setIsEditing} />
+    </div>
+    )
+  }
+
+  const EmptyState = () => {
+    <div className="channel-empty__container">
+      <p className="channel-empty__first"> This is the beginning of your chat history. </p>
+      <p className="channel-empty__second"> Send messages, attachment, links, emojis, and More! </p>
+    </div>
+  }
+
+
   return (
-    <div>
-      ChannelContainer
+    <div className='channel__container'>
+      <Channel 
+      EmptyStateIndicator={EmptyState}
+      Message={(messageProps, i) => <TeamMessage key={i} {...messageProps} />}
+      >
+        <ChannelInner setIsEditing={setIsEditing} />
+      </Channel>
     </div>
   )
 }
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -65,6 +65,7 @@ const ChannelListContainer = () => {
             />
           )} 
       />
+
        <ChannelList 
           filters={{}}
           channelRenderFilterFn={ () => {}}
